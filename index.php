60cf065ab3b6262d03f82e6716732756c719f228
syahraqagr <32156061+syahraqagr@users.noreply.github.com>
2018-06-05 13:29:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ copyright @ medantechno.com
 
 require_once('./line_class.php');
 
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //sesuaikan 
-$channelSecret = 'YOUR-CHANNEL-SECRET-CODE';//sesuaikan
+$channelAccessToken = '3mTkZrX0oPra46uMe0zaYhgZisp3K+iEMuh0nXCTXPG31lKTXcD7XWRYKZ3RwVQGYeY/UjHCZvH5WyXDKblWGlMLDeudwM2Zig6kChQSr92WfKEZRqL1jiwqSy6Yzn1liYzZvM84ye2dF+RnOC3ohAdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = 'ef937ac3033ada91ca7688f0c0633250';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $userId 	= $client->parseEvents()[0]['source']['userId'];
@@ -22,8 +22,7 @@ $pesan_datang = $message['text'];
 //pesan bergambar
 if($message['type']=='text')
 {
-	if($pesan_datang=='Hi')
-	{
+	if($pesan_datang=='Hi') {
 		
 		
 		$balas = array(
@@ -33,6 +32,57 @@ if($message['type']=='text')
 										'type' => 'text',					
 										'text' => 'Halo'
 									)
+							)
+						);
+				
+	}
+	if($pesan_datang=='Hai') {
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+array (
+  'type' => 'template',
+  'altText' => 'this is a image carousel template',
+  'template' => 
+  array (
+    'type' => 'image_carousel',
+    'columns' => 
+    array (
+      0 => 
+      array (
+        'imageUrl' => 'http://preview.ibb.co/bMcQco/Pics_Art_06_05_01_02_05.jpg',
+        'action' => 
+        array (
+          'type' => 'postback',
+          'label' => 'Buy',
+          'data' => 'action=buy&itemid=111',
+        ),
+      ),
+      1 => 
+      array (
+        'imageUrl' => 'http://preview.ibb.co/bMcQco/Pics_Art_06_05_01_02_05.jpg',
+        'action' => 
+        array (
+          'type' => 'message',
+          'label' => 'Yes',
+          'text' => 'yes',
+        ),
+      ),
+      2 => 
+      array (
+        'imageUrl' => 'http://preview.ibb.co/bMcQco/Pics_Art_06_05_01_02_05.jpg',
+        'action' => 
+        array (
+          'type' => 'uri',
+          'label' => 'View detail',
+          'uri' => 'http://example.com/page/222',
+        ),
+      ),
+    ),
+  ),
+)
 							)
 						);
 				
